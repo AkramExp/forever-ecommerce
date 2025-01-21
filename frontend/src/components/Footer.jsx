a71b5bbb0e42cc47640ccc08514574d6b2e5c57d
akramexp <akram.exp02@gmail.com>
2025-01-21 18:27:26 +0530

    added footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} alt="logo" className="mb-5 w-32" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            quaerat, repudiandae maiores ducimus tenetur sequi numquam
+            voluptatibus ea tempora eos a saepe. Numquam quis porro, blanditiis
+            quia omnis commodi? Voluptas.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91-123-456-7890</li>
+            <li>contact@forever.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-center text-sm">
+          Copyright @2024 - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
