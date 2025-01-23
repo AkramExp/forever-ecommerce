364e3d1952452884b7e513ee6a23c49fb7c8170b
Akram Ali <akram.exp02@gmail.com>
2025-01-23 18:22:59 +0530

    added cart items functions

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/assets";
+import { ToastContainer, toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,6 +9,40 @@ const ShopContextProvider = ({ children }) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId, size) => {
+    if (!size) {
+      toast.error("Select product size");
+      return;
+    }
+
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let count = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        count += cartItems[items][item];
+      }
+    }
+
+    return count;
+  };
 
   return (
     <ShopContext.Provider
@@ -19,6 +54,9 @@ const ShopContextProvider = ({ children }) => {
         setSearch,
         showSearch,
         setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount,
       }}
     >
       {children}
