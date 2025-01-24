29e12fba1c11ecf70402862b18c0fe2204df75cd
Akram Ali <akram.exp02@gmail.com>
2025-01-24 23:09:17 +0530

    added mongodb and cloudinary connections

---
+++ backend/config/mongodb.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database not connected " + error);
+  }
+};
+
+export default connectDB;
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "../config/mongodb.js";
+import connectCloudinary from "../config/cloudinary.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+connectDB();
+connectCloudinary();
+
+app.get("/", (req, res) => {
+  console.log("helo");
+
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
