a737911476eadd4091671456c95e8ed5378171ba
Akram Ali <akram.exp02@gmail.com>
2025-01-27 15:10:42 +0530

    added login and logout functionality in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+
+export const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <>
-        <Navbar />
-        <hr className="border-gray-300" />
-        <div className="w-full flex">
-          <Sidebar />
-        </div>
-      </>
+      <ToastContainer />
+      {token ? (
+        <>
+          <Navbar setToken={setToken} />
+          <hr className="border-gray-300" />
+          <div className="w-full flex">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Login setToken={setToken} />
+      )}
     </div>
   );
 };
