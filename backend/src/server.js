ab65193a2588e33d362b7295f89c48c76cc00d92
Akram Ali <akram.exp02@gmail.com>
2025-01-27 19:37:26 +0530

    minor changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/user.routes.js";
 import productRouter from "./routes/product.routes.js";
+import cartRouter from "./routes/cart.routes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(port, () => {
   console.log("Server started on port " + port);
