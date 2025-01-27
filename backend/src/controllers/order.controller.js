ed92b58e15db87d791c2fcd5d09ba14e9476663c
Akram Ali <akram.exp02@gmail.com>
2025-01-27 23:01:17 +0530

    added cod order backend

---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,24 @@
+import Order from "../models/order.model";
+
+export const placeOrderCod = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    await Order.create(orderData);
+
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
