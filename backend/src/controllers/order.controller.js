854edf71c29128b09bb4de30be68246ae7a5e056
Akram Ali <akram.exp02@gmail.com>
2025-01-29 18:32:19 +0530

    added stripe payment in api

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,4 +1,10 @@
 import Order from "../models/order.model.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const currency = "$";
+const delivery_fee = 10;
 
 export const placeOrderCod = async (req, res) => {
   try {
@@ -17,6 +23,61 @@ export const placeOrderCod = async (req, res) => {
     await Order.create(orderData);
 
     res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const order = await Order.create(orderData);
+
+    const lineItems = items.map((item) => ({
+      priceData: {
+        currency,
+        productData: { name: item.name },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    lineItems.push({
+      priceData: {
+        currency,
+        productData: {
+          name: "Delivery Charges",
+        },
+        unit_amount: delivery_fee * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${order._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${order._id}`,
+      line_items: lineItems,
+      mode: "payment",
+    });
+
+    res.json({
+      success: true,
+      message: "Order Placed",
+      session_url: session.url,
+    });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
@@ -50,7 +111,6 @@ export const allOrders = async (req, res) => {
 export const toggleOrderState = async (req, res) => {
   try {
     const { orderId, orderState } = req.body;
-    console.log(req.body);
 
     const order = await Order.findById(orderId);
 
