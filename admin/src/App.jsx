47a9c373eb130e6c1feac41b6d6f0cfb179011d1
Akram Ali <akram.exp02@gmail.com>
2025-01-30 22:55:29 +0530

    redirect home to "/add" in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
@@ -31,7 +31,8 @@ const App = () => {
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/" element={<Navigate to="/add" replace />} />
+                <Route index path="/add" element={<Add token={token} />} />
                 <Route path="/list" element={<List token={token} />} />
                 <Route path="/orders" element={<Orders token={token} />} />
               </Routes>
