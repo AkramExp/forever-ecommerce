cbd4b2a48e920624b0a5274770434bd63303be72
akramexp <akramali.jafri02@gmail.com>
2025-04-08 13:31:59 +0530

    minor changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://forever-akramexp.vercel.app",
+  })
+);
 connectDB();
 connectCloudinary();
 
